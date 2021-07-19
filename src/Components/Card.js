2fc7ebe20abdd59cc71f096cded9b47a2778023a
vrishanka <55500396+vrishanka@users.noreply.github.com>
2021-07-19 21:07:39 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,22 +8,28 @@ function Card({ title, imgUrl, description }) {
         <div className="card-container">
             <div className="img">
                 <img src={imgUrl } alt="no-photo"/>
-            </div>
+            
             <div className="card-content">
                 <div className="title">
                     <h1>{title}</h1>
-                </div>
+                
                 <div className="description">
                     {description}
-                </div>
-            </div>
+                
+            
             
             <div className="btn">
                 <button >
                         <a>View more</a>
                     </button>
-                </div>
+                
         </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
     )
 }
 
